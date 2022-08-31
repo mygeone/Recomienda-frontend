aa9f1ecae1328e06dfb4b80f599d2ac1289c177c
myge <miguel.contreras1@mail.udp.cl>
2022-08-30 20:23:14 -0400

    a

---
+++ app-next/components/Map.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef, ReactElement } from "react";
+
+function Map({ center, zoom }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    new window.google.maps.Map(ref.current, {
+      center,
+      zoom,
+    });
+  });
+
+  return <div ref={ref} id="map" />;
+}
+
+export default Map;
---
+++ app-next/pages/map.js
@@ -0,0 +1,103 @@
+
+import Navbar from "../components/Navbar";
+import Map from "../components/Map";
+
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+
+export default function Example() {
+
+    const render = (status) => {
+      switch (status) {
+        case Status.LOADING:
+          //return <Spinner />;
+        case Status.FAILURE:
+          //return <ErrorComponent />;
+        case Status.SUCCESS:
+          return <Map />;
+      }
+    };
+
+      const center = { lat: -34.397, lng: 150.644 };
+      const zoom = 4;
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto p-4 sm:px-6 ">
+        <div className="hidden sm:block" aria-hidden="true">
+          <div className="py-5">
+            <div className="border-t border-gray-200" />
+          </div>
+        </div>
+
+        <div className="mt-10 sm:mt-0">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="mt-5 md:mt-0 md:col-span-1">
+              <form action="#" method="POST">
+                <div className="shadow overflow-hidden sm:rounded-md">
+                  <div className="px-4 py-5 bg-white sm:p-6">
+                    <div className="grid grid-cols-6 gap-6 py-5">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="country"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Región
+                        </label>
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        >
+                          <option>United States</option>
+                          <option>Canada</option>
+                          <option>Mexico</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="grid grid-cols-6 gap-6 pt-3">
+                      <div className="col-span-6 sm:col-span-3">
+                        <label
+                          htmlFor="country"
+                          className="block text-sm font-medium text-gray-700"
+                        >
+                          Comuna
+                        </label>
+                        <select
+                          id="country"
+                          name="country"
+                          autoComplete="country-name"
+                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        >
+                          <option>United States</option>
+                          <option>Canada</option>
+                          <option>Mexico</option>
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Wrapper
+        apiKey={"AIzaSyCy9Ou1TmA8jOoEkof3vTxe-qlnmFnxxDM"}
+        render={render}
+      >
+        <Map center={center} zoom={zoom}  />
+      </Wrapper>
+    </>
+  );
+}
